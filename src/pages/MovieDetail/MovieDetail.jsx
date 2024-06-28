5081d62a286d7073f7a41fd596d7b799b7a4d5fe
Vahagn Ghazaryan <vahagn.ghazaryan2001@gmail.com>
2024-06-28 19:56:02 +0400

    added movie details title

--- src/pages/MovieDetail/MovieDetail.jsx
+++ src/pages/MovieDetail/MovieDetail.jsx
@@ -1,3 +1,18 @@
+import axios from "axios";
+
+
+export const movieDetailsById = async (movieId) => {
+  const { data } = await axios.get(
+    `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`
+  );
+  return data;
+};
+
 export const MovieDetail = () => {
-  return <div>MovieDetail</div>;
+
+  return (
+    <section>
+      <h1>{movieId.original_title}</h1>
+    </section>
+  )
 };
