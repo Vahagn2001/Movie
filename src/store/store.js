a029dcec8ed8a99252a5e2a8347ece040a349506
Valod Baghdasaryan <baghdasaryanvalod2@gmail.com>
2024-07-06 20:35:28 +0400

    Migrate trendingMovies to react-query

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import trendingMovies from "./slices/trendingMovies";
 import movieDetail from "./slices/movieDetail";
 import searchMovie from "./slices/searchMovieSlice";
 
 export default configureStore({
   reducer: {
-    trendingMovies,
     movieDetail,
     searchMovie,
   },
--- src/store/thunks/trendingMovies.js
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { getMoviesByPopularity } from "../../api/discoverMovie";
-
-export const fetchTrendingMovies = createAsyncThunk(
-  "trendingMovies/fetch",
-  getMoviesByPopularity
-);
